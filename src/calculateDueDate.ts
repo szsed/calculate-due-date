c4c4cf0dbd7e6cb5b6c4d6f91c5a26921885b339
Szabolcs Sed <szabolcs.sed@gmail.com>
2021-07-10 10:14:35 +0200

    pass test for submit date/time working hours validation

--- src/calculateDueDate.ts
+++ src/calculateDueDate.ts
@@ -2,6 +2,18 @@ const validateSubmitDateTime = (submitDateTime: Date): void => {
   if (!submitDateTime || !(submitDateTime instanceof Date)) {
     throw new TypeError('Submit date/time must be a Date object');
   }
+  if (submitDateTime.getHours() < 9) {
+    throw new RangeError('Submit date/time must be between 9:00AM and 17:00PM');
+  }
+  if (submitDateTime.getHours() > 17) {
+    throw new RangeError('Submit date/time must be between 9:00AM and 17:00PM');
+  }
+  if (submitDateTime.getHours() === 17 && submitDateTime.getMinutes() > 0) {
+    throw new RangeError('Submit date/time must be between 9:00AM and 17:00PM');
+  }
+  if (submitDateTime.getDay() === 6 || submitDateTime.getDay() === 0) {
+    throw new RangeError('Submit date/time must be between 9:00AM and 17:00PM');
+  }
 };
 
 const isInteger = (n: number) => n === Math.floor(n);
