e2daf4fb89de4a4369d0da9e8a255a165a12b8cc
Szabolcs Sed <szabolcs.sed@gmail.com>
2021-07-10 10:06:40 +0200

    add test for submit date/time working hours validation

--- test/jest/calculateDueDate.test.ts
+++ test/jest/calculateDueDate.test.ts
@@ -2,7 +2,7 @@ import calculateDueDate from '../../src/calculateDueDate';
 
 describe('calculateDueDate', () => {
   describe('input/output validation', () => {
-    const testSubmitDateTime = new Date('2021-07-10T09:30:00');
+    const testSubmitDateTime = new Date('2021-07-09T09:30:00');
     const testTurnaroundTime = 5;
     it('should return a Date object', () => {
       const result = calculateDueDate(testSubmitDateTime, testTurnaroundTime);
@@ -14,6 +14,20 @@ describe('calculateDueDate', () => {
       expect(() => calculateDueDate(null, testTurnaroundTime)).toThrow();
       expect(() => calculateDueDate(Date.now() as unknown as Date, testTurnaroundTime)).toThrow();
       expect(() => calculateDueDate({} as unknown as Date, testTurnaroundTime)).toThrow();
+    });
+
+    it('should reject submit date/time values outside working hours', () => {
+      const earlyHourSubmitDateTime = new Date('2021-07-09T08:59:00');
+      const lateHourSubmitDateTime = new Date('2021-07-09T18:00:00');
+      const lateMinuteSubmitDateTime = new Date('2021-07-09T17:01:00');
+      const saturdaySubmitDateTime = new Date('2021-07-10T09:30:00');
+      const sundaySubmitDateTime = new Date('2021-07-11T09:30:00');
+
+      expect(() => calculateDueDate(earlyHourSubmitDateTime, testTurnaroundTime)).toThrow();
+      expect(() => calculateDueDate(lateHourSubmitDateTime, testTurnaroundTime)).toThrow();
+      expect(() => calculateDueDate(lateMinuteSubmitDateTime, testTurnaroundTime)).toThrow();
+      expect(() => calculateDueDate(saturdaySubmitDateTime, testTurnaroundTime)).toThrow();
+      expect(() => calculateDueDate(sundaySubmitDateTime, testTurnaroundTime)).toThrow();
     });
 
     it('should validate the turnaround time parameter', () => {
