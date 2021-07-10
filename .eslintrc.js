9e5aee743662582feb4981e06d00988f244e977f
Szabolcs Sed <szabolcs.sed@gmail.com>
2021-07-10 09:12:08 +0200

    setup project

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+};
