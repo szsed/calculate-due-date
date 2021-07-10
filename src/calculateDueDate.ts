63ca482d183e500d0a7b66841e437e513ff8be3f
Szabolcs Sed <szabolcs.sed@gmail.com>
2021-07-10 09:57:31 +0200

    pass tests for input validation

--- src/calculateDueDate.ts
+++ src/calculateDueDate.ts
@@ -1,3 +1,17 @@
-const calculateDueDate = (...args: any[]) => new Date();
+const calculateDueDate = (submitDateTime: Date, turnaroundTime: number) => {
+  if (!submitDateTime || !(submitDateTime instanceof Date)) {
+    throw new TypeError('Submit date/time must be a Date object');
+  }
+
+  if (!(typeof turnaroundTime === 'number')) {
+    throw new TypeError('Turnaround time must be a number');
+  }
+
+  if (turnaroundTime <= 0 || turnaroundTime !== Math.floor(turnaroundTime)) {
+    throw new RangeError('Turnaround time must be positive integer');
+  }
+
+  return new Date();
+};
 
 export default calculateDueDate;
